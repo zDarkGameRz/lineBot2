567e5edad1c58d01008b6f951f3d1cfc82d60192
zDarkGameRz <51397163+zDarkGameRz@users.noreply.github.com>
2019-07-03 14:17:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (userText == 'สวัสดี') :
+        sendText(user,'ว่าไงสหาย')
+    elif (userText == 'โซเวียต') :
+        sendText(user,'อยู่ในใจ')
+    else :
+       sendText(user,'Red Army')
     return '',200
 
 def sendText(user, text):
