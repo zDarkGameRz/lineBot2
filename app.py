ed692631a6a12d309cbec37fddbe08b067da8feb
zDarkGameRz <51397163+zDarkGameRz@users.noreply.github.com>
2019-07-09 18:05:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     sendText(user,userText)
-    if (userText == 'สวัสดี') :
-        sendText(user,'ว่าไงสหาย')
-    elif (userText == 'โซเวียต') :
-        sendText(user,'อยู่ในใจ')
-    else :
-        sendText(user,'Red Army')
+#    if (userText == 'สวัสดี') :
+#        sendText(user,'ว่าไงสหาย')
+#    elif (userText == 'โซเวียต') :
+#        sendText(user,'อยู่ในใจ')
+#    else :
+#        sendText(user,'Red Army')
     return '',200
 
 def sendText(user, text):
